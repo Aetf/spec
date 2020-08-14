9b0df90bddac98771fee22ae39eac6ba80b359f2
Aetf <aetf@unlmited-code.works>
2020-08-14 19:43:39 -0400

    Assign groups to hosts

--- inventory_plugins/resolved_local.py
+++ inventory_plugins/resolved_local.py
@@ -10,19 +10,24 @@ DOCUMENTATION = r'''
     description: Returns localhost's real hostname for easier matching in host vars
     options:
       plugin:
-          description: Name of the plugin
-          required: true
-          choices: ['resolved_local']
+        description: Name of the plugin
+        required: true
+        choices: ['resolved_local']
+      groups:
+        description: Assign groups to hosts, key is regex, value is list of groups
+        required: false
     extends_documentation_fragment:
       - inventory_cache
 '''
 
 
+import re
 import socket
 
 from ansible.plugins.inventory import BaseInventoryPlugin, Constructable, Cacheable
-from ansible.errors import AnsibleError
+from ansible.errors import AnsibleError, AnsibleParserError
 from ansible.module_utils._text import to_native
+from ansible.inventory.group import to_safe_group_name
 
 
 class InventoryModule(BaseInventoryPlugin, Constructable, Cacheable):
@@ -50,25 +55,53 @@ class InventoryModule(BaseInventoryPlugin, Constructable, Cacheable):
         cache_needs_update = user_cache_setting and not cache
 
         # attempt to read the cache if inventory isn't being refreshed and the user has caching enabled
-        hostname = None
+        data = None
         if attempt_to_read_cache:
             try:
-                hostname = self._cache[cache_key]
+                data = self._cache[cache_key]
             except KeyError:
                 # This occurs if the cache_key is not in the cache or if the cache_key expired, so the cache needs to be updated
                 cache_needs_update = True
 
-        if hostname is None:
+        if data is None:
             # resovle hostname
             try:
-                hostname = socket.gethostname()
+                data = self._resolve_hosts()
             except Exception as e:
                 raise AnsibleError('Failed to get local hostname: {}'.format(to_native(e)))
 
         if cache_needs_update:
             # set the cache
-            self._cache[cache_key] = hostname
+            self._cache[cache_key] = data
 
-        self.inventory.add_host(hostname)
-        # make sure it's connected via local
-        self.inventory.set_variable(hostname, 'ansible_connection', 'local')
+        for hostname, groups in data:
+            self.inventory.add_host(hostname)
+            # make sure it's connected via local
+            self.inventory.set_variable(hostname, 'ansible_connection', 'local')
+            for group in groups:
+                group = to_safe_group_name(group, force=True)
+                # ensure group exists, use sanitized name
+                group = self.inventory.add_group(group)
+                # add host to group
+                self.inventory.add_child(group, hostname)
+
+    def _resolve_hosts(self):
+        hostname = socket.gethostname()
+        matched_groups = []
+        for key, groups in (self.get_option('groups') or {}).items():
+            try:
+                ptn = re.compile(key)
+            except Exception as e:
+                raise AnsibleParserError(
+                    "Could not add {} to {}: {}".format(
+                        hostname,
+                        groups,
+                        to_native(e)
+                    )
+                )
+            if ptn.search(hostname):
+                matched_groups.extend(groups)
+
+        return [
+            (hostname, matched_groups)
+        ]
